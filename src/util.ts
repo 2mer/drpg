7e225e3665a871aa64f39322b746fd1e9d0968c7
2mer <kenyweb7@gmail.com>
2024-07-17 23:19:18 +0300

    feat: added more shops

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
 export function block<T extends () => any>(fn: T) {
 	return fn();
 }
+
+export type ValueOrCompute<T, TCompute extends (...args: any) => T = (p: T) => T> = T | TCompute;
+export type ValueOrComputeHandler<T extends ValueOrCompute<any, any>> = T extends ValueOrCompute<any, infer R> ? R : never;
+export function handleValueOrCompute<T extends ValueOrCompute<any, any>>(v: T, ...rest: Parameters<ValueOrComputeHandler<T>>) {
+	if (typeof v === 'function') return v(...rest);
+
+	return v;
+}
