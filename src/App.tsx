83d6fff9a4088c43142606622abdc5976371186f
2mer <kenyweb7@gmail.com>
2024-07-19 14:02:59 +0300

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import Stage from './components/Stage';
+import oopsImg from './assets/ui/oops.png';
+import { PIXEL_RESOLUTION } from './constants';
+
+function fallbackRender({ error }: { error: any }) {
+	return (
+		<div
+			className='flex flex-col bg-slate-800 h-full w-full justify-center items-center gap-4'
+			role='alert'
+			// @ts-ignore
+			style={{ '--pixel-res': `${PIXEL_RESOLUTION}px` }}
+		>
+			<img
+				className='w-res h-res [image-rendering:pixelated]'
+				src={oopsImg}
+			/>
+			<p className='text-white'>Something went wrong</p>
+			<pre className='text-red-500'>{error.message}</pre>
+		</div>
+	);
+}
 
 function App() {
-	return <Stage />;
+	return (
+		<ErrorBoundary fallbackRender={fallbackRender}>
+			<Stage />
+		</ErrorBoundary>
+	);
 }
 
 export default App;
